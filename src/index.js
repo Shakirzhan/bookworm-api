0b0187629d6ece2f6a49e1e9a7097d77bf2e3e04
Shakirzhan <samarbaev99@mail.ru>
2019-10-20 17:33:26 +0300

    v 4

--- src/index.js
+++ src/index.js
@@ -3,18 +3,17 @@ import path from 'path';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import Promise from 'bluebird';
 
 import auth from './routes/auth';
 
 dotenv.config();
 const app = express();
 app.use(bodyParser.json());
+mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB, {useMongoClient: true });
 
 app.use('/api/auth', auth);
-// app.post("/api/auth", (req, res) => {
-//   res.status(400).json({ errors: { global: "Invalid credentials" } });
-// });
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
